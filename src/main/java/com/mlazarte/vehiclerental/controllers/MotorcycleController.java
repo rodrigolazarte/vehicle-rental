0d9fde365d4d4db06673089fdbcba3107b26fbae
Martin Lazarte <martin.lazarte@endava.com>
2022-11-14 16:28:39 -0300

    Added functionality for MotorcycleService

--- src/main/java/com/mlazarte/vehiclerental/controllers/MotorcycleController.java
+++ src/main/java/com/mlazarte/vehiclerental/controllers/MotorcycleController.java
@@ -1,46 +1,87 @@
 package com.mlazarte.vehiclerental.controllers;
 
+import com.mlazarte.vehiclerental.dto.MotorcycleDto;
+import com.mlazarte.vehiclerental.exceptions.VehicleInUseException;
 import com.mlazarte.vehiclerental.models.Motorcycle;
-import com.mlazarte.vehiclerental.repositories.MotorcycleRepository;
+import com.mlazarte.vehiclerental.services.MotorcycleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/motorcycles")
+@RequestMapping("/api")
 @CrossOrigin
 public class MotorcycleController {
+
     @Autowired
-    private MotorcycleRepository repository;
+    private MotorcycleService motorcycleService;
 
     @GetMapping(
+            value = "/motorcycles",
             produces = "application/json"
     )
     public ResponseEntity<List<Motorcycle>> getAllMotorcycles() {
-        List<Motorcycle> motorcycles = repository.findAll();
+        List<Motorcycle> motorcycles = motorcycleService.getAllMotorcycles();
         return ResponseEntity.ok().body(motorcycles);
     }
 
     @GetMapping(
-            value = "/{id}",
+            value = "/motorcycles/{id}",
             produces = "application/json"
     )
     public ResponseEntity<Motorcycle> getMotorcycleById(@PathVariable Integer id) {
-        Optional<Motorcycle> motorcycle = repository.findById(id);
-        if (motorcycle.isPresent()) return ResponseEntity.ok().body(motorcycle.get());
-        else return ResponseEntity.notFound().build();
+        Motorcycle motorcycle = motorcycleService.getMotorcycleById(id);
+
+        if (motorcycle != null)
+            return ResponseEntity.ok().body(motorcycle);
+        else
+            return ResponseEntity.notFound().build();
     }
 
     @PostMapping(
+            value = "/motorcycles",
             produces = "application/json"
     )
-    public ResponseEntity<Motorcycle> saveMotorcycle(@RequestBody Motorcycle motorcycle) {
-        repository.save(motorcycle);
+    public ResponseEntity<Motorcycle> saveMotorcycle(@RequestBody MotorcycleDto motorcycleDto) {
+        Motorcycle motorcycle = motorcycleService.saveNewMotorcycle(motorcycleDto);
         URI uri = URI.create("/api/motorcycles/" + motorcycle.getVehicleId());
         return ResponseEntity.created(uri).body(motorcycle);
+    }
+
+    @PutMapping(
+            value = "/motorcycles/{id}",
+            produces = "application/json"
+    )
+    public ResponseEntity<Motorcycle> updateMortorcycle(
+            @PathVariable Integer id,
+            @RequestBody MotorcycleDto motorcycleDto
+    ) {
+        Motorcycle motorcycle = motorcycleService.updateMotorcycle(id, motorcycleDto);
+        if (motorcycle != null)
+            return ResponseEntity.ok().body(motorcycle);
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping(
+            value = "/motorcycles/{id}",
+            produces = "application/json"
+    )
+    public ResponseEntity<Motorcycle> deleteMotorcycle(@PathVariable Integer id) {
+        Motorcycle motorcycle = motorcycleService.deleteMotorcycle(id);
+        if (motorcycle != null)
+            if (motorcycle.isAvailable())
+                return ResponseEntity.ok().body(motorcycle);
+            else
+                throw new VehicleInUseException(
+                        HttpStatus.BAD_REQUEST,
+                        "The motorcycle with id " + motorcycle.getVehicleId() + " is used in a rental."
+                );
+        else
+            return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/mlazarte/vehiclerental/services/MotorcycleService.java
+++ src/main/java/com/mlazarte/vehiclerental/services/MotorcycleService.java
@@ -19,21 +19,50 @@ public class MotorcycleService {
     @Autowired
     private ModelMapper modelMapper;
 
-    public List<Motorcycle> getAllMotorcycles(){
+    public List<Motorcycle> getAllMotorcycles() {
         return motorcycleRepository.findAll();
     }
 
-    public Motorcycle getMotorcycleById(Integer id){
+    public Motorcycle getMotorcycleById(Integer id) {
         Optional<Motorcycle> motorcycle = motorcycleRepository.findById(id);
 
         return motorcycle.orElse(null);
     }
 
-    public Motorcycle saveNewMotorcycle(MotorcycleDto motorcycleDto){
+    public Motorcycle saveNewMotorcycle(MotorcycleDto motorcycleDto) {
         Motorcycle motorcycle = modelMapper.map(motorcycleDto, Motorcycle.class);
         motorcycle.setAvailable(true);
         motorcycleRepository.save(motorcycle);
 
         return motorcycle;
+    }
+
+    public Motorcycle updateMotorcycle(Integer id, MotorcycleDto motorcycleDto) {
+        var oldMotorcycle = motorcycleRepository.findById(id);
+
+        if (oldMotorcycle.isPresent()) {
+            var updatedMotorcycle = modelMapper.map(motorcycleDto, Motorcycle.class);
+
+            updatedMotorcycle.setVehicleId(oldMotorcycle.get().getVehicleId());
+            updatedMotorcycle.setAvailable(oldMotorcycle.get().isAvailable());
+            updatedMotorcycle.setRental(oldMotorcycle.get().getRental());
+            motorcycleRepository.save(updatedMotorcycle);
+
+            return updatedMotorcycle;
+        } else
+            return null;
+    }
+
+    public Motorcycle deleteMotorcycle(Integer id){
+        var motorcycleToDelete = motorcycleRepository.findById(id);
+
+        if (motorcycleToDelete.isPresent()) {
+            if (motorcycleToDelete.get().isAvailable()) {
+                motorcycleRepository.delete(motorcycleToDelete.get());
+            }
+        } else
+            return null;
+
+        return motorcycleToDelete.get();
     }
 }
