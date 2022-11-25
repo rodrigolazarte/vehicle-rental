2e4068f6c8a2f55ece213b62ba727dfd173495b7
Martin Lazarte <martin.lazarte@endava.com>
2022-11-25 15:16:22 -0300

    Fixed ElectroScooterService interface

--- src/main/java/com/mlazarte/vehiclerental/services/ElectricScooterService.java
+++ src/main/java/com/mlazarte/vehiclerental/services/ElectricScooterService.java
@@ -5,12 +5,13 @@ import com.mlazarte.vehiclerental.models.ElectricScooter;
 import com.mlazarte.vehiclerental.repositories.ElectricScooterRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ElectricScooterService {
-
 
     @Autowired
     private ElectricScooterRepository scooterRepository;
